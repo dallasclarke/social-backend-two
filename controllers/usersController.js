f15e20dd1567b88ee5a6ed574f9dd995823504e6
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-13 23:24:35 -0800

    Made userController and made registeration function

---
+++ controllers/usersController.js
@@ -0,0 +1,58 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+
+const User = require("../models/User");
+
+module.exports = {
+  register: async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password, birthday } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists!" }] });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+        birthday,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: "8h" },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error!");
+    }
+  },
+};
