2ae6e7361df39faa5cdf3a8971e6f2b947cf636c
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-13 23:05:38 -0800

    Connected to MongoDB successfully

--- config/db.js
+++ config/db.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
 	const connection = await mongoose.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
-		useCreateIndex: true,
 		useUnifiedTopology: true,
-		useFindAndModify: false,
 	});
 
 	console.log(`MongoDB Connected: ${connection.connection.host}`);
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const usersRouter = require("./routes/users");
+
+connectDB();
 
 const app = express();
 
@@ -11,19 +14,19 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", usersRouter);
 
 if (process.env.NODE_ENV === "development") {
-	app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 
 const PORT = process.env.PORT || 8080;
 
 const server = app.listen(
-	PORT,
-	console.log(
-		`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
-	)
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
 );
 
 process.on("unhandledRejection", (err, promise) => {
-	console.log(`Error: ${err.message}`);
-	server.close(() => process.exit(1));
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
 });
