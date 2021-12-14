47b63b7e5d68a9f5598141450194114ae297e60f
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-13 23:29:35 -0800

    Set up router to post in users.js, just have to test if route is successful in postman

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,19 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 
-router.get("/", (req, res) => res.send("Test Working!!!!"));
+const { register } = require("../controllers/usersController");
+
+router.post(
+  "/",
+  [
+    check("name", "Name is required!").not().isEmpty(),
+    check("email", "Please include a valid email!").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters!"
+    ).isLength({ min: 6 }),
+  ],
+  register
+);
 
 module.exports = router;
