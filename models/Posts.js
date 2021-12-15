dbdcf16c62aa806280156b30480a16f1aafae72b
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-14 21:39:47 -0800

    Post model created.

---
+++ models/Posts.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  text: { type: String, required: true },
+  name: { type: String },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+  comments: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+      text: { type: String, required: true },
+      name: { type: String },
+      date: { type: Date, default: Date.now },
+    },
+  ],
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("post", PostSchema);
