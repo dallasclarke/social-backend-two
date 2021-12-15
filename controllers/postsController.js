281f102a6e8336f40458a9f86f3d876bd51bb4a9
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-14 22:02:54 -0800

    getAllPosts function created in postsController. Will test createPost & getAllPosts after creating routes.

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -29,4 +29,14 @@ module.exports = {
       res.status(500).send("Server error");
     }
   },
+  getAllPosts: async (req, res) => {
+    try {
+      const posts = await Post.find().sort({ date: -1 });
+
+      res.json(posts);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  },
 };
