55dd899cadcfd056bc743ee45ff6d78d12debb14
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-20 23:52:51 -0800

    Had to fix typo from fullName to name in users model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, trim: true, unique: true, required: true },
   password: { type: String, required: true },
-  fullName: { type: String, required: true },
+  name: { type: String, required: true },
   birthday: { type: Date, required: true },
   dateJoined: { type: Date, default: Date.now },
 });
